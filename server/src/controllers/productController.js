4109b0070100b4db0ae661289a29a385ab59e642
emonShimoul <emonshimoul@gmail.com>
2026-04-27 18:08:18 +0600

    creat api for getting a single product

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -29,3 +29,18 @@ exports.getProducts = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// get single product
+exports.getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- server/src/routes/productRoutes.js
+++ server/src/routes/productRoutes.js
@@ -4,11 +4,14 @@ const {
   createProduct,
   getProducts,
 } = require("../controllers/productController");
+const { getProductById } = require("../controllers/productController");
 
 const protect = require("../middleware/authMiddleware");
 const admin = require("../middleware/adminMiddleware");
 
 router.post("/", protect, admin, createProduct);
 router.get("/", getProducts);
+router.get("/:id", getProductById);
+
 
 module.exports = router;
