a14542fa846ebb8852da0ed876f980ff6451ac23
emonShimoul <emonshimoul@gmail.com>
2026-05-10 20:15:10 +0600

    slight changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-gray-300 mt-16">
+    <footer className="bg-gray-900 text-gray-300">
 
       <div className="max-w-7xl mx-auto px-4 py-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
 
